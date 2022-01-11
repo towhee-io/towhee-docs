cc22a87e6135f28009431f29d4238c8f0dfbc7ca
tumao <yan.wang@zilliz.com>
2022-01-11 10:36:37 +0800

    Fix broken edit url
    Signed-off-by: tumao <yan.wang@zilliz.com>

--- setup.js
+++ setup.js
@@ -1,8 +1,10 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 
 const setup = () => {
   // copy sidebar file
   fs.copyFileSync('towhee/docs/sidebars.js', './sidebars.js');
+  // copy docs folder
+  fs.copy('towhee/docs', './docs');
 };
 
 setup();
