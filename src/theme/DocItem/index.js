a3069d38c8c0da1c786bb2e945031890acb1292b
tumao <yan.wang@zilliz.com>
2022-01-13 18:13:57 +0800

    Add docs search
    Signed-off-by: tumao <yan.wang@zilliz.com>

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -86,9 +86,15 @@ export default function DocItem(props) {
                 See https://github.com/facebook/docusaurus/pull/4882#issuecomment-853021120
                 */}
                 {shouldAddTitle && <MainHeading>{title}</MainHeading>}
+
                 {/* use authors component in blog template */}
                 {authors && authors.length > 0 && (
-                  <BlogPostAuthors authors={authors} assets={assets} />
+                  <>
+                    <h5>Authors:</h5>
+                    <BlogPostAuthors authors={authors} assets={assets} />
+                    {/* divider */}
+                    <div className={styles.divider}></div>
+                  </>
                 )}
                 <DocContent />
               </div>
